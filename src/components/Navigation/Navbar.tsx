cc4605aa32dffa4f378657eef6c0fde07f3827e1
Hannes Alexandersson <135141396+HannesAlexandersson@users.noreply.github.com>
2024-09-19 14:18:24 +0200

    adding login button if user isnt logged in

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,9 +1,9 @@
 import { FC, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { pageLinks } from './Navlinks';
+
 import { useUser } from "@/context/userContext";
 import Button from '../Buttons';
-
 interface NavbarProps {
   currentPath: string;
 }
@@ -11,75 +11,76 @@ interface NavbarProps {
 const Navbar: FC<NavbarProps> = ({ currentPath }) => {
   const [language, setLanguage] = useState('sv');
   const { user, isLoading, signOut } = useUser();
-console.log(user)
+  const navigate = useNavigate();
+
+  const handleToSignIn = () => {
+    navigate('/sign-in');
+  };
+
   return (
     <nav className="bg-white w-full h-auto shadow-md px-4 mx-auto pt-3 pb-4">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="flex flex-col">
-          {/* Top row */}
-          <div className="flex justify-between items-center mb-4">
-            <a href="/">
-              <img src="ccblogo.png" alt="Logo" className="h-auto w-[180px] align-middle" />
-            </a>
-            <div className="flex items-center space-x-4">
-              {!isLoading && (
-                <>
-                  {user ? (
-                    <>
-                      <span>CCBUILD</span>
-                      <span>TJÄNSTER</span>
-                      <span>MARKNADSPLATSEN</span>
-                      <span>PRODUKTBANKEN</span>
-                      <Button size='small' onClick={signOut}>Logga ut</Button>
-                      <span>{user.aud} ▼</span>
-                    </>
-                  ) : (
-                    <>
-                      <Link to="/sign-in">
-                        <Button size='small'>Logga in</Button>
-                      </Link>
-                      <Link to="/sign-up">
-                        <Button size='small'>Registrera</Button>
-                      </Link>
-                    </>
-                  )}
-                </>
+        <div className="flex justify-between items-center h-16">
+        <div className="flex items-center">
+          <a href="/" >
+            <img src="ccblogo.png" alt="Logo" className="h-auto w-[180px] align-middle mr-4" />
+          </a>
+        </div>
+          
+          <div className='flex flex-col gap-1'>
+            <div className="hidden md:flex md:flex-wrap gap-1 space-x-4 mt-4">
+              {pageLinks.slice(0, 5).map((link) => (
+                <Link
+                  key={link.title}
+                  to={link.path}
+                  className={`text-gray-700 hover:text-gray-900 ${currentPath === link.path ? 'font-bold' : ''}`}
+                >
+                  {link.title}
+                </Link>
+              ))}
+            </div>
+            <div className="hidden md:flex md:flex-wrap gap-1 space-x-4">
+              {pageLinks.length > 5 && (
+              <>
+                {pageLinks.slice(5).map((link) => (
+                  <Link
+                    key={link.title}
+                    to={link.path}
+                    className={`text-gray-700 hover:text-gray-900 ${currentPath === link.path ? 'font-bold' : ''}`}
+                  >
+                    {link.title}
+                  </Link>
+                ))}   
+              </>         
               )}
+            
+            </div>
+          </div>
+          <div className='flex flex-col gap-1'>
+            <div className="flex items-center space-x-4">
               <select
                 value={language}
                 onChange={(e) => setLanguage(e.target.value)}
                 className="bg-gray-100 border border-gray-300 bg-none rounded px-2 py-1"
               >
-                <option value="sv">SV</option>
-                <option value="en">EN</option>
+                <option value="sv">🇸🇪</option>
+                <option value="en">🇬🇧</option>
               </select>
-            </div>
-          </div>
-          
-          {/* thisonly show if user is logged in */}
-          {!isLoading && user && (
-            <div className="bg-navy-blue text-white py-2 px-4 flex justify-between items-center">
-              <div className="flex space-x-4">
-                {pageLinks.map((link) => (
-                  <Link
-                    key={link.title}
-                    to={link.path}
-                    className={`hover:text-gray-300 ${currentPath === link.path ? 'font-bold' : ''}`}
-                  >
-                    {link.title.toUpperCase()}
-                  </Link>
-                ))}
-              </div>
               <div className="relative">
                 <input
                   type="text"
-                  placeholder="Sök produkter, kategorier.."
-                  className="bg-white text-black border border-gray-300 rounded pl-8 pr-2 py-1"
+                  placeholder="Sök"
+                  className="bg-gray-100 border border-gray-300 rounded pl-8 pr-2 py-1"
                 />
                 <img src="/search.svg" alt="Search" className="absolute left-2 top-1/2 transform -translate-y-1/2 text-gray-400" />
+                
               </div>
             </div>
-          )}
+            <div className='flex justify-end items-center' >
+              {!user && <Button size='small' className='w-1/2' onClick={handleToSignIn}>Logga in</Button>}
+              {!isLoading && user && <Button size='small' className='w-1/2' onClick={signOut}>Logga ut</Button>}
+            </div>
+          </div>
         </div>
       </div>
     </nav>
