d61f948b4d0d478780f9b6f0596463aa2e290312
William Davidson <wj.davidson@outlook.com>
2024-09-20 08:51:47 +0200

    refactored progress step component and addded correct colors

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,11 +4,9 @@ import Input from "@/components/Input";
 import {
   ProgressSteps,
   ProgressStepsCard,
-  ProgressStepsCardNumber,
   Status,
 } from "@/components/products/ProgressSteps";
 //import { useUser } from "@/context/userContext";
-import { cn } from "@/lib/utils";
 import { FC, useState } from "react";
 import Form from "@/components/Form";
 const defaultSteps: { title: string; status: Status }[] = [
@@ -22,7 +20,7 @@ const defaultSteps: { title: string; status: Status }[] = [
   },
   {
     title: "Egenskaper",
-    status: null,
+    status: "error",
   },
   {
     title: "Produktinformation",
@@ -35,32 +33,24 @@ const defaultSteps: { title: string; status: Status }[] = [
 ];
 
 const Home: FC = () => {
-  const [step, setStep] = useState(2);
+  const [step, setStep] = useState(3);
   const [selected, setSelected] = useState<Date>();
 
   /* const { user, isLoading, signOut } = useUser();
- */
+   */
   return (
     <div>
-     {/*  {!isLoading && user && <Button onClick={signOut}>Logga ut</Button>} */}
+      {/*  {!isLoading && user && <Button onClick={signOut}>Logga ut</Button>} */}
       <ProgressSteps>
         {defaultSteps.map((s, i) => (
           <ProgressStepsCard
-            active={step === i}
-            onClick={() => setStep(i)}
+            name={s.title}
+            number={i + 1}
             status={s.status}
+            active={i === step}
             key={s.title}
-          >
-            <ProgressStepsCardNumber
-              className={cn({
-                "border-black text-black": step === i || s.status,
-              })}
-              number={i + 1}
-            />
-            <p className="text-nowrap text-ellipsis overflow-hidden">
-              {s.title}
-            </p>
-          </ProgressStepsCard>
+            onClick={() => setStep(i)}
+          />
         ))}
       </ProgressSteps>
       <DatePicker selected={selected} setSelected={setSelected} />
