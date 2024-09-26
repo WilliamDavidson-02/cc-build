cf5c4a616afbdf76a6f8a87c69282197f77aeabb
William Davidson <wj.davidson@outlook.com>
2024-09-26 09:07:11 +0200

    added url support for file preview

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -6,12 +6,18 @@ type TestProps = {};
 const Test: FC<TestProps> = ({}) => {
   const [uploadedFiles, setUploadedFiles] = useState<File[]>([]);
 
+  const uploadedUrls = [
+    "http://127.0.0.1:54321/storage/v1/object/public/files/Wooded%20Area%20with%20Man%20and%20Dog.jpg",
+    "http://127.0.0.1:54321/storage/v1/object/public/files/Screenshot%202024-09-16%20at%2014.32.06.png",
+  ];
+
   return (
     <FileUpload
       title="ksopekfop"
       uploadedFiles={uploadedFiles}
       setUploadedFiles={(files) => setUploadedFiles(files)}
       className="max-w-[400px] mx-auto"
+      urls={uploadedUrls}
     />
   );
 };
